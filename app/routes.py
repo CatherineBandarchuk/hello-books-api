da09d98c3c9cf22d5105353b1e7dc40450d414c9
Catherine Bandarchuk <kate@sealucky.com>
2022-12-20 21:37:24 -0800

    Add delete route

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,27 @@ from flask import Blueprint, jsonify, make_response, abort, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+# Helper functions
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+    book = Book.query.get(book_id)
+
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
+            
+    return book
+
+# Routes functions
 @books_bp.route("", methods=["POST"])
 def create_book():
     request_body = request.get_json()
+    
+    if "title" not in request_body or "description" not in request_body:
+        abort(make_response({"message":f"Invalid Request."}, 400))        
+
     new_book = Book(title=request_body["title"],
                     description=request_body["description"])
 
@@ -29,28 +47,37 @@ def read_all_books():
         )
     return jsonify(books_response)
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        abort(make_response({"message":f"book {book_id} invalid"}, 400))
-    book = Book.query.get(book_id)
-
-    if not book:
-        abort(make_response({"message":f"book {book_id} not found"}, 404))
-            
-    return book
-
-    
-        
 @books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
+def read_one_book(book_id):
     book = validate_book(book_id)
 
     return {
         "id": book.id,
         "title": book.title,
-        "description": book.description,
+        "description": book.description
     }
 
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
 
+    request_body = request.get_json()
+    
+    if "title" not in request_body or "description" not in request_body:
+        abort(make_response({"message":f"Invalid Request."}, 400))     
+    
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully updated")
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully deleted")
