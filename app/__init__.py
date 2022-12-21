0e4d05d700fbc3a844a7bcdc838d9ea1aefc7c51
Catherine Bandarchuk <kate@sealucky.com>
2022-12-20 20:55:49 -0800

    Add handle_book(book_id) route

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,8 @@ migrate = Migrate()
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development"
 
     # Import models here
     from app.models.book import Book
